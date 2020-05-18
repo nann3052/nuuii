32939610aef2945707fcb2d881a3f646dcc58526
woodbully <60384319+woodbully@users.noreply.github.com>
2020-05-18 10:39:27 +0200

    Link og reset

--- js/script.js
+++ js/script.js
@@ -15,3 +15,46 @@ function showMobileNav() {
     
 }
 
+
+//Up-arrow animation
+const backToTopBtn = document.querySelector("#back-to-top-btn");
+const downArrow = document.querySelector("#down-arrow");
+
+window.addEventListener("scroll", scrollFunction);
+
+function scrollFunction(){
+    if (window.pageYOffset < 2010){
+        downArrow.style.display = "block";
+    }
+    else{
+        downArrow.style.display = "none";
+    }
+}
+
+window.addEventListener("scroll", scrollUpFunction);
+
+function scrollUpFunction() {
+    if (window.pageYOffset > 2010){
+        backToTopBtn.style.display = "block";
+    }
+    else{
+        backToTopBtn.style.display = "none";
+    }
+}
+
+
+backToTopBtn.addEventListener("click", backToTop);
+
+function backToTop(){
+    window.scroll(0, 0);
+}
+
+
+function backToTop(){
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    });
+}
+
