f48c0d86be1f026bf03d1007594be54838a9ad6a
woodbully <60384319+woodbully@users.noreply.github.com>
2020-05-20 14:39:32 +0200

    Sendes til murat

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 function showMobileNav() {
-    
+
     let showMenu = document.querySelector("#nav-links-mobile");
 
     if (showMenu.style.width === "100vw") {
@@ -7,12 +7,12 @@ function showMobileNav() {
     } else {
         showMenu.style.width = "100vw";
     }
-    
+
     // burger animation at toggle
     const burger = document.querySelector("#burger");
-    
+
     burger.classList.toggle("toggle");
-    
+
 }
 
 
@@ -22,10 +22,9 @@ const backToTopBtn = document.querySelector("#back-to-top-btn");
 window.addEventListener("scroll", scrollUpFunction);
 
 function scrollUpFunction() {
-    if (window.pageYOffset > 500){
+    if (window.pageYOffset > 500) {
         backToTopBtn.style.display = "block";
-    }
-    else{
+    } else {
         backToTopBtn.style.display = "none";
     }
 }
@@ -33,12 +32,12 @@ function scrollUpFunction() {
 
 backToTopBtn.addEventListener("click", backToTop);
 
-function backToTop(){
+/*function backToTop() {
     window.scroll(0, 0);
-}
+}*/
 
 
-function backToTop(){
+function backToTop() {
     window.scrollTo({
         top: 0,
         left: 0,
@@ -52,11 +51,10 @@ const getIngrBtnElement = document.getElementById("getIngr");
 getIngrBtnElement.addEventListener("click", getIngr);
 
 function getIngr() {
-    fetch('json/ingredienser.json')
-        .then((res) => res.json())
+    fetch('ingredienser.txt')
+        .then((res) => res.text())
         .then((data) => {
-         let output = '';
-            document.getElementById("output").innerHTML = data;
+           console.log(data); document.getElementById("output").innerHTML = data;
         })
 }
 
@@ -66,17 +64,15 @@ const faq = document.querySelector(".faq-answer")
 
 let i;
 
-for(i=0; i <faq.length;i++) {
-    faq [i].onclick = function() {
+for (i = 0; i < faq.length; i++) {
+    faq[i].onclick = function () {
         this.classList.toggle("active")
-        
+
         const panel = this.nextElementSibling
-        
-        if(panel.style.maxHeight){
+
+        if (panel.style.maxHeight) {
             panel.style.maxHeight = null;
-        }
-        
-        else{
+        } else {
             panel.style.maxHeight = panel.scrollHeight + "px"
         }
     }
