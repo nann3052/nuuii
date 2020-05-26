2586a6e0ab8433d641268be4200caba31d14cc63
woodbully <60384319+woodbully@users.noreply.github.com>
2020-05-26 09:05:14 +0200

    fixed bck-to-top arrow

---
+++ js/arrow.js
@@ -0,0 +1,28 @@
+//Up-arrow animation
+const backToTopBtn = document.querySelector("#back-to-top-btn");
+
+window.addEventListener("scroll", scrollUpFunction);
+
+function scrollUpFunction() {
+    if (window.pageYOffset > 500) {
+        backToTopBtn.style.display = "block";
+    } else {
+        backToTopBtn.style.display = "none";
+    }
+}
+
+
+backToTopBtn.addEventListener("click", backToTop);
+
+function backToTop() {
+    window.scroll(0, 0);
+}
+
+
+function backToTop() {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    });
+}
--- js/script.js
+++ js/script.js
@@ -17,36 +17,7 @@ function showMobileNav() {
 
 /*
 
-//Up-arrow animation
-const backToTopBtn = document.querySelector("#back-to-top-btn");
 
-window.addEventListener("scroll", scrollUpFunction);
-
-function scrollUpFunction() {
-    if (window.pageYOffset > 500) {
-        backToTopBtn.style.display = "block";
-    } else {
-        backToTopBtn.style.display = "none";
-    }
-}
-
-
-backToTopBtn.addEventListener("click", backToTop);
-
-function backToTop() {
-    window.scroll(0, 0);
-}
-
-
-
-
-function backToTop() {
-    window.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: "smooth"
-    });
-}
 */
 
 
